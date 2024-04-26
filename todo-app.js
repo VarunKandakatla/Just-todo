0562365f7c6108399fa9e029fb5b2392ad297d3a
Varun Kandakatla <varunkumarkandakatla@gmail.com>
2024-04-26 18:03:49 +0530

    modi

--- todo-app.js
+++ todo-app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-app.listen(3000);
-app.use(express.json());
 
+app.use(express.json());
+port = process.env.PORT || 3000;
+app.listen(port);
 todoList = [];
 
 getTodo().then((data)=>{
