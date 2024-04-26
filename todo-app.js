88f756124872fe26f9bb8def9c78979a62ffe388
Varun Kandakatla <varunkumarkandakatla@gmail.com>
2024-04-26 19:15:58 +0530

    remived files

--- todo-app.js
+++ todo-app.js
@@ -7,9 +7,9 @@ port = process.env.PORT || 3000;
 app.listen(port);
 todoList = [];
 
-getTodo().then((data)=>{
-    todoList = data;
-})
+// getTodo().then((data)=>{
+//     todoList = data;
+// })
 
 app.get('/todos',(req,res)=>{
     res.status(200).json({Todos : todoList});
@@ -19,28 +19,36 @@ app.get('/',(req,res)=>{
     res.status(200).send('Hello from express');
 })
 
-app.get('/:title',(req,res)=>{
+app.post('/:title',(req,res)=>{
     const task = req.params.title;
     todoList.push(task);
-    addTodo().then(()=>{
-        res.status(200).json({message : "Task Added!"});
-    })
+    res.status(200).json({message : "Task Added!"});
 })
 
-function getTodo()
-{
-    return new Promise((resolve,reject)=>{
-        fs.readFile('./todos.json','utf-8',(err,data)=>{
-            todoList = JSON.parse(data);
-            resolve(todoList);
-        })
-    })
-}
+app.put('/update/:id',(req,res)=>{
+    todoList[req.params.id] = req.query.Q;
+    res.status(200).send("Updated!");
+})
 
-function addTodo(){
-    return new Promise((resolve,reject)=>{
-        fs.writeFile('./todos.json',JSON.stringify(todoList),(err)=>{
-            resolve();
-        });
-    })
-}
+app.get('/get',(req,res)=>{
+    res.status(200).json({list : todoList});
+})
+
+
+// function getTodo()
+// {
+//     return new Promise((resolve,reject)=>{
+//         fs.readFile('./todos.json','utf-8',(err,data)=>{
+//             todoList = JSON.parse(data);
+//             resolve(todoList);
+//         })
+//     })
+// }
+
+// function addTodo(){
+//     return new Promise((resolve,reject)=>{
+//         fs.writeFile('./todos.json',JSON.stringify(todoList),(err)=>{
+//             resolve();
+//         });
+//     })
+// }
